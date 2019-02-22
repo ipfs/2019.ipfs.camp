dc96dcddd69a661b532e825d9b972d337eba75bc
Chris Waring <chris@wwaves.co>
2019-02-22 12:25:36 +0000

    refactor stars

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './stars.css'
 
-export const Stars: React.FC = () => (
+type StarsProps = {
+  className?: string
+  style?: React.CSSProperties
+}
+
+export const Stars: React.FC<StarsProps> = ({ className, style }) => (
   <React.Fragment>
-    <div className="stars" />
-    <div className="stars-cl" />
+    <aside
+      className={`absolute top-0 bottom-0 left-0 right-0 z-00 overflow-hidden h-100 ${className}`}
+      style={style}
+    >
+      <div className="stars" />
+      <div className="stars-cl" />
+    </aside>
   </React.Fragment>
 )
