534e68b9c7a308115194de70903abf59f3189e67
Chris Waring <chris@wwaves.co>
2019-02-25 22:37:11 +0000

    remove blog/test pages

--- static.config.js
+++ static.config.js
@@ -12,24 +12,4 @@ export default {
   getSiteData: () => ({
     title: 'IPFS Camp 2019',
   }),
-  // getRoutes: async () => {
-  //   const { data: posts } = await axios.get(
-  //     'https://jsonplaceholder.typicode.com/posts',
-  //   )
-  //   return [
-  //     {
-  //       path: '/blog',
-  //       getData: () => ({
-  //         posts,
-  //       }),
-  //       children: posts.map(post => ({
-  //         path: `/post/${post.id}`,
-  //         component: 'src/containers/Post',
-  //         getData: () => ({
-  //           post,
-  //         }),
-  //       })),
-  //     },
-  //   ]
-  // },
 }
