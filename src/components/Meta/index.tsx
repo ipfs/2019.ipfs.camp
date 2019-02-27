f733d7a4620c51a1eb5f4c82e299ccefdeba920e
Chris Waring <chris@wwaves.co>
2019-02-27 23:43:12 +0000

    add favicons

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -29,6 +29,9 @@ export const DefaultMeta: React.FC<DefaultMetaProps> = ({ children }) => (
       type="application/rss+xml"
       title="IPFS Blog"
     />
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+
     {children}
   </Head>
 )
