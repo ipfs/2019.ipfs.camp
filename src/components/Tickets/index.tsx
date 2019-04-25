d0023ae9e8988b493fa74047fedcccaa2e0c43a9
Chris Waring <chris@wwaves.co>
2019-04-25 18:05:39 +0100

    break out ticket types

---
+++ src/components/Tickets/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Button } from '@components/Buttons'
+
+const Benefits = () => (
+  <ul className="tl pa0">
+    <li>All talks, workshops and sessions</li>
+    <li>Breakfast, lunch and dinner</li>
+    <li>Airport or train station transfers</li>
+    <li>Awesome group activities</li>
+  </ul>
+)
+
+const Card: React.FC = ({ children }) => (
+  <div className="br2 shadow-4 pa4 ma3 tc">{children}</div>
+)
+
+const Price: React.FC = ({ children }) => (
+  <p>
+    <span className="primary3 f3 tc db mv2 nowrap">{children}</span>
+  </p>
+)
+
+const ApplicationBtn = () => (
+  <Button block href="https://goo.gl/forms/GmzwgMzfR0RUpfs13">
+    <div className="tc">Apply</div>
+  </Button>
+)
+
+const Shared = () => (
+  <Card>
+    <h2>Shared Twin Room</h2>
+    <Price>795 €</Price>
+    <Benefits />
+    <ApplicationBtn />
+  </Card>
+)
+const Private = () => (
+  <Card>
+    <h2>Private Double Room</h2>
+    <Price>995 €</Price>
+    <Benefits />
+    <ApplicationBtn />
+  </Card>
+)
+
+export const Tickets = () => (
+  <div className="flex flex-column flex-row-ns space-between">
+    <Private />
+    <Shared />
+  </div>
+)
