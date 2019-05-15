554ff68c245034fd0ff66d9749eee35ce1546e58
Chris Waring <chris@wwaves.co>
2019-05-15 11:48:41 +0100

    #11 add ipfs cluster to projects grid

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -6,6 +6,7 @@ import ipld from './ipld-logo.svg'
 import libp2p from './libp2p-logo.svg'
 import multiformats from './multiformats-logo.svg'
 import protoschool from './protoschool-logo.svg'
+import ipfsCluster from './ipfs-cluster-logo.svg'
 
 const ProjectGrid = styled.div`
   background: url(${grid}) no-repeat top left;
@@ -42,6 +43,9 @@ const ProjectGrid = styled.div`
   .x5 {
     left: 467px;
   }
+  .x6 {
+    left: 538px;
+  }
 `
 
 type ProjectProps = {
@@ -51,19 +55,34 @@ type ProjectProps = {
 export const Projects: React.FC<ProjectProps> = props => (
   <ProjectGrid {...props} className="dn db-ns">
     <a href="https://ipfs.io/">
-      <img src={ipfs} className="ipfs-logo y1 x1 h4 grow" />
+      <img alt="IPFS" src={ipfs} className="ipfs-logo y1 x1 h4 grow" />
     </a>
     <a href="https://libp2p.io/">
-      <img src={libp2p} className="libp2p-logo y2 x2 h4 grow" />
+      <img alt="libp2p" src={libp2p} className="libp2p-logo y2 x2 h4 grow" />
     </a>
     <a href="https://ipld.io">
-      <img src={ipld} className="ipld-logo y1 x3 h4 grow" />
+      <img alt="ipld" src={ipld} className="ipld-logo y1 x3 h4 grow" />
     </a>
-    <a href="https://multiformats.io/">
-      <img src={multiformats} className="multiformats-logo y2 x4 h4 grow" />
+    <a href="https://cluster.ipfs.io/">
+      <img
+        alt="IPFS Cluster"
+        src={ipfsCluster}
+        className="ipfsCluster-logo y2 x4 h4 grow"
+      />
     </a>
     <a href="https://proto.school/">
-      <img src={protoschool} className="protoschool-logo y1 x5 h4 grow" />
+      <img
+        alt="ProtoSchool"
+        src={protoschool}
+        className="protoschool-logo y1 x5 h4 grow"
+      />
+    </a>
+    <a href="https://multiformats.io/">
+      <img
+        alt="multiformats"
+        src={multiformats}
+        className="multiformats-logo y2 x6 h4 grow"
+      />
     </a>
   </ProjectGrid>
 )
