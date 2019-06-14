ed5d2a3c221f3dfc852b684a3653063427c1acb6
Chris Waring <chris@wwaves.co>
2019-06-14 14:40:29 +0200

    extract to human components

---
+++ src/components/Humans/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Humans } from 'types/humans'
+
+const HumanImage = styled.img`
+  min-width: 128px;
+  min-height: 128px;
+  transition: all 0.5s;
+`
+
+type HumanGridProps = {
+  humans: Humans
+}
+
+export const HumanGrid: React.FC<HumanGridProps> = ({ humans }) => (
+  <div className="flex flex-wrap justify-center">
+    {humans.map(human => (
+      <Human key={human.title} {...human} />
+    ))}
+  </div>
+)
+
+export const Human = (human: any) => (
+  <div className="mw4 ma2">
+    <div className="tc lh-copy">
+      <a href={`https://github.com/${human.github}`}>
+        <HumanImage
+          className="br3 bg-animate grow"
+          src={`https://github.com/${human.github}.png?size=200`}
+        />
+      </a>
+      <p className="f6">{human.title}</p>
+      <RolePills roles={human.roles} />
+    </div>
+  </div>
+)
+
+type RolePillsProps = {
+  roles: string[]
+}
+
+export const RolePills: React.FC<RolePillsProps> = ({ roles }) => (
+  <>
+    {roles.map((role: string) => (
+      <span key={role} className="f7 pa2 ma1 br-pill bg-neutral5">
+        {role}
+      </span>
+    ))}
+  </>
+)
--- src/containers/Speakers.tsx
+++ src/containers/Speakers.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useRouteData } from 'react-static'
 import { Page } from '../layouts'
-import styled from 'styled-components'
+import { HumanGrid } from '@components/Humans'
+import { Humans } from 'types/humans'
 
 export default () => {
   const {
@@ -9,13 +10,7 @@ export default () => {
     title,
     meta,
   }: {
-    speakers: [
-      {
-        github: string
-        title: string
-        roles: string[]
-      },
-    ]
+    speakers: Humans
     title: string
     meta: {}
   } = useRouteData()
@@ -23,58 +18,25 @@ export default () => {
     <Page meta={meta} title={title}>
       <h2>Production Team</h2>
       <div className="flex flex-wrap justify-center">
-        {speakers
-          .filter(speaker => !!speaker.roles.includes('core'))
-          .sort(() => Math.random() - 0.5)
-          .map(speaker => (
-            <Speaker key={speaker.title} {...speaker} />
-          ))}
+        <HumanGrid
+          humans={speakers
+            .filter(speaker => !!speaker.roles.includes('core'))
+            .sort(() => Math.random() - 0.5)}
+        />
       </div>
       <h2>Speakers & Trainers</h2>
       <div className="flex flex-wrap justify-center">
-        {speakers
-          .filter(
-            speaker =>
-              (speaker.roles.includes('team') &&
-                !speaker.roles.includes('core')) ||
-              speaker.roles.includes('contrib'),
-          )
-          .sort(() => Math.random() - 0.5)
-          .map(speaker => (
-            <Speaker key={speaker.title} {...speaker} />
-          ))}
+        <HumanGrid
+          humans={speakers
+            .filter(
+              speaker =>
+                (speaker.roles.includes('team') &&
+                  !speaker.roles.includes('core')) ||
+                speaker.roles.includes('contrib'),
+            )
+            .sort(() => Math.random() - 0.5)}
+        />
       </div>
     </Page>
   )
 }
-
-const SpeakerImage = styled.img`
-  min-width: 128px;
-  min-height: 128px;
-  transition: all 0.5s;
-`
-
-const Speaker = (speaker: any) => (
-  <div className="mw4 ma2">
-    <div className="tc lh-copy">
-      <a href={`https://github.com/${speaker.github}`}>
-        <SpeakerImage
-          className="br3 bg-animate grow"
-          src={`https://github.com/${speaker.github}.png?size=200`}
-        />
-      </a>
-      <p className="f6">{speaker.title}</p>
-      <RolePills roles={speaker.roles} />
-    </div>
-  </div>
-)
-
-const RolePills = ({ roles }: { roles: string[] }) => (
-  <>
-    {roles.map((role: string) => (
-      <span key={role} className="f7 pa2 ma1 br-pill bg-neutral5">
-        {role}
-      </span>
-    ))}
-  </>
-)
