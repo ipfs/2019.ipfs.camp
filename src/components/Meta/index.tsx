8c96c0d06d8515df7a689bddf93bc6b060ef72db
dietrich ayala <autonome@gmail.com>
2019-06-27 17:53:48 +0200

    icon update

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -34,8 +34,12 @@ export const DefaultMeta: React.FC<DefaultMetaProps> = ({ children }) => (
       type="application/rss+xml"
       title="IPFS Blog"
     />
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 
     {children}
   </Head>
